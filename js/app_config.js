dd8b95ea82c01d4823eee7fdadc81b4ab9215c0c
Kamil Skala <kskala.prace@gmail.com>
2015-06-16 08:30:33 +0200

    Rewrite production url

--- js/app_config.js
+++ js/app_config.js
@@ -2,7 +2,7 @@
  * Created by ext96215 on 3.6.2015.
  */
 var configData = {
-        productionURL: 'http://csas.cz/resources/',
+        productionURL: 'http://productionURL/resources/',
         developmentURL: 'http://private-1bd61-firstresource.apiary-mock.com',
         isDevelopment:true
     };
